5a9a1628b06416d6c08b715f1905e4c2bee38652
Amir Sabanovic <asabanovic3@gmail.com>
2023-06-04 19:33:41 +0200

    Add initial scripts and manifest

--- background_script.js
+++ background_script.js
@@ -1,1 +1,11 @@
 // JavaScript code to execute in background
+function logTabAudible(tabs) {
+	console.log(tabs);
+}
+
+function listener(message) {
+	console.log(message);
+	browser.tabs.query({ currentWindow: true }, logTabAudible);
+}
+
+browser.runtime.onMessage.addListener(listener);
--- content_script.js
+++ content_script.js
@@ -1,1 +1,12 @@
 // JavaScript to execute after page load
+function update() {
+	setTimeout(function() {
+		browser.runtime.sendMessage({ event: "" });
+	}, 0);
+}
+
+var video = document.getElementsByTagName("video")[0];
+
+["play", "playing", "pause"].forEach(function (e) {
+	video.addEventListener(e, update, false);
+});
